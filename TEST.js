4d2cdba5937c6bde98fc5686b487ee1da6586e10
renzosantamaria <renzo.santamaria@outlook.com>
2020-11-26 23:42:27 +0100

    .

--- TEST.js
+++ TEST.js
@@ -1,87 +1,85 @@
-let allBeerData = []
 
-async function getAllData() {
-    let request = await fetch('https://api.punkapi.com/v2/beers?page=2&per_page=80')
-    let answer = await request.json()
+// let string1 = 'abcd'
+// let string2 = 'cd'
 
-    // console.log(answer[0].ingredients.malt)
-    for (let i = 0; i < answer.length; i++) {
-        answer[i].ingredients.malt
-        console.log(answer[i].ingredients.malt)
-    }
 
-}
-// getAllData()
+// let solution = (str, ending) => {return ending == str.slice(str.length - ending.length) ? true : false}
 
-   
-//Search by malt
-async function fetchByMalt(malt){
-    const request = await fetch (`https://api.punkapi.com/v2/beers?malt=${malt}`)
-    const answer = await request.json()
-    // console.log(answer)
-    return answer
-}
+// console.log(solution(string1, string2));
+    
 
-let chosenMalt = "amber"
-fetchByMalt(chosenMalt)
-
-//-----------------------------------------
-
-//1. Search by hops
-async function fetchByHops(hops){
-    const request = await fetch (`https://api.punkapi.com/v2/beers?hops=${hops}`)
-    const answer = await request.json()
-    console.log(answer)
-    return answer
-}
-
-let chosenHops = "Saaz"
-fetchByHops(chosenHops)
-
-//-----------------------------------------
-
-//2. Search by ABV GREATER than xxxx
-async function fetchByAbvGreaterThan(abvG){
-    const request = await fetch (`https://api.punkapi.com/v2/beers?abv_gt=${abvG}`)
-    const answer = await request.json()
-    console.log(answer)
-    return answer
-}
-
-let chosenAbvGreaterThan = "5"
-fetchByAbvGreaterThan(chosenAbvGreaterThan)
-
-//-----------------------------------------
-
-//3. Search by ABV LESS than xxxx
-async function fetchByAbvLessThan(abvL){
-    const request = await fetch (`https://api.punkapi.com/v2/beers?abv_lt=${abvL}`)
-    const answer = await request.json()
-    console.log(answer)
-    return answer
-}
-
-let chosenAbvLessThan = "5"
-fetchByAbvLessThan(chosenAbvLessThan)
+//--------------------------------------------------
+//--------------------------------------------------
 
 
 
 
 
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+//let arraycito = [':)', ';(', ';}', ':-D'];
+//let arraycito = [';D', ':-(', ':-)', ';~)'];
+//let arraycito = [';]', ':[', ';*', ':$', ';-D'];
+//let arraycito = [];
+
+// let countSmileys = (array) => {
+//     let counter = 0;
+//     array.forEach(element => {
+//         element.length == 2 && (element[0] == ":" || element[0] == ";") && (element[1] == ")" || element[1] == "D") ? counter++ : counter
+//         element.length == 3 && (element[0] == ":" || element[0] == ";") && (element[1] == "-" || element[1] == "~") && (element[2] == ")" || element[2] == "D") ? counter++ : counter        
+//     })
+//     return counter
+// }
+// console.log(countSmileys(arraycito))
 
 
 
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
 
 
 
+// Write a function that takes an array of numbers (integers for the tests) and a target number.
+// It should find two different items in the array that, when added together, give the target value.
+// The indices of these items should then be returned in a tuple like so: (index1, index2).
 
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
 
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers;
+//     target will always be the sum of two different items from that array).
 
+// let numbers = [ 1, 2, 3, 4, 5]
+// let targ = 4
 
+// function twoSum(nums, target) {
+//     let answer = []
+//     for (let i = nums.length -1 ; i >= 0; i--) {
+//         const element = nums[i];
 
+//         nums.forEach(elemento => {
+//             //console.log(element)
+//             if (element + elemento == target) {
+//                 //console.log(element, elemento)
+//                 answer.push([element, elemento])
+//             }
+//         })
+        
+//     }
+//     return answer
+    
+//   }
+//  console.log(twoSum(numbers, targ));
+//twoSum(numbers, targ)
 
-
-
+  //twoSum [1, 2, 3] 4 === (0, 2)
 
 
 
@@ -89,61 +87,9 @@ fetchByAbvLessThan(chosenAbvLessThan)
 
 
 
+//   TEST FOR VIDEO FULLSCREEN
 
 
 
 
 
-
-
-
-
-
-
-// Acidulated Malt
-// Amber
-// Brown 
-// Black Malt
-// Carafa Special Malt Type 1 
-// Carafa Special Malt Type 2
-// Carafa Special Malt Type 3
-// CaraAroma
-// Caramunich
-// Cascara
-// Chocolate
-// Crystal 150 
-// Caramalt
-// Dark Crystal
-// Dark Crystal 350-400
-// Dextrin malt
-// Extra Pale 
-// Extra Pale - Spring Blend
-// Flaked Oats
-// Ginger Root
-// Honey
-// Lactose
-// Maris Otter
-// Maris Otter Extra Pale
-// Munich 
-// Organic Lager
-// Organic Caramalt
-// Organic Munich
-// Organic Chocolate
-// Pilsner
-// Roasted Barley
-// Roasted Malt
-// Rye
-// Smoked Warminster
-// Special W
-// Torrified Wheat
-// Weyermann Beech Smoked
-// Wheat
-// Wheat Malt
-// White Cane Sugar
-
-
-   
-
-
-
-//Name, Hops, Malt, Brewed before/after, Abv
